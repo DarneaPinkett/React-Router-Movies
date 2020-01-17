b38d666c8a88ebef38b0a7be9dc283b692381f16
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-01-17 09:43:59 -0500

    P2

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import {Route} from "react-router-dom";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
+  const [savedList, setSavedList] = useState ([]);
+
+  const addToSavedList = movie => {
+    setSavedList([...savedList, movie]);
+  };
 
   return (
     <div>
